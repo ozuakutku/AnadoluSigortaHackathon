3a8ffc840b2f17d27682531c294c2a8ccc5d411a
Kadir Yalçın <kadirrryalcin@gmail.com>
2023-12-13 20:26:31 +0300

    olusturdum

--- lib/pages/accident_datail_page.dart
+++ lib/pages/accident_datail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sigortamcepte/pages/haritadeneme.dart';
 
 class AccidentDetailPage extends StatefulWidget {
   @override
@@ -9,6 +10,11 @@ class AccidentDetailPage extends StatefulWidget {
 
 class _AccidentDetailPageState extends State<AccidentDetailPage> {
   TextEditingController aciklamaController = TextEditingController();
+  TextEditingController isimController = TextEditingController();
+  TextEditingController soyisimController = TextEditingController();
+  TextEditingController adresController = TextEditingController();
+  TextEditingController plakaController = TextEditingController();
+
   String? selectedLocation;
   bool mevcutKonumuKullan = true;
 
@@ -20,40 +26,37 @@ class _AccidentDetailPageState extends State<AccidentDetailPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Veri Girişi'),
+        actions: [TextButton(onPressed: () {}, child: Text("Otomatik Doldur"))],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: ListView(
           children: [
-            ElevatedButton(
-              onPressed: () async {
-                // Harita sayfasını aç ve konumu seç
-                String? location = await _selectLocation(context);
-                if (location != null) {
-                  setState(() {
-                    selectedLocation = location;
-                  });
-                }
-              },
-              child: Text('Konum Seç'),
-            ),
-            SizedBox(height: 16.0),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue),
-              ),
-              child: TextField(
-                controller: aciklamaController,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  labelText: 'Hasar Açıklaması',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(10.0),
+            CustomTextField(isimController: isimController, labelText: "İsim"),
+            CustomTextField(
+                isimController: isimController, labelText: "Soyisim"),
+            Text("Plaka"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: CustomTextField(
+                      isimController: isimController, labelText: "Plaka"),
                 ),
-              ),
+                Expanded(
+                  child: CustomTextField(
+                      isimController: isimController, labelText: "Plaka"),
+                ),
+                Expanded(
+                  child: CustomTextField(
+                      isimController: isimController, labelText: "Plaka"),
+                ),
+              ],
             ),
+            CustomTextField(isimController: isimController, labelText: "Adres"),
+            getLocationButton(context),
+            SizedBox(height: 16.0),
+            DetailBox(aciklamaController: aciklamaController),
             SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
@@ -76,16 +79,92 @@ class _AccidentDetailPageState extends State<AccidentDetailPage> {
     );
   }
 
-  Future<String?> _selectLocation(BuildContext context) async {
+  ElevatedButton getLocationButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        // Harita sayfasını aç ve konumu seç
+        String? location = await _selectLocation(context);
+        if (location != null) {
+          setState(() {
+            selectedLocation = location;
+          });
+        }
+      },
+      child: Text('Konum Seç'),
+    );
+  }
+
+  Future<String?> _selectLocation(
+    BuildContext context,
+  ) async {
     // Harita sayfasını aç ve konumu seç
     final String? selectedLocation = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => accMapPage(),
+        builder: (context) => LocationMapPage(),
       ),
     );
 
     return selectedLocation;
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.isimController,
+    required this.labelText,
+  });
+
+  final TextEditingController isimController;
+  final String labelText;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(color: Colors.blue),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: labelText,
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(10.0),
+          ),
+          controller: isimController,
+        ),
+      ),
+    );
+  }
+}
+
+class DetailBox extends StatelessWidget {
+  const DetailBox({
+    super.key,
+    required this.aciklamaController,
+  });
+
+  final TextEditingController aciklamaController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(color: Colors.blue),
+      ),
+      child: TextField(
+        controller: aciklamaController,
+        maxLines: 5,
+        decoration: InputDecoration(
+          labelText: 'Hasar Açıklaması',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(10.0),
+        ),
+      ),
+    );
   }
 }
 
@@ -114,63 +193,34 @@ class OutputPage extends StatelessWidget {
       ),
     );
   }
-}
 
-class accMapPage extends StatefulWidget {
-  @override
-  _accMapPageState createState() => _accMapPageState();
-}
+  // Mevcut konumu almak için kullanılacak fonksiyon
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-class _accMapPageState extends State<accMapPage> {
-  late GoogleMapController mapController;
-  LatLng? selectedLatLng;
-  Set<Marker> markers = {};
+    // Konum servisi etkin mi kontrol et
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Eğer kapalı ise kullanıcıyı konum servisini açmaya yönlendir
+      serviceEnabled = await Geolocator.openLocationSettings();
+      if (!serviceEnabled) {
+        // Eğer kullanıcı konum servisini açmazsa hata ver
+        throw 'Konum servisi kapalı';
+      }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Konum Seç'),
-      ),
-      body: GoogleMap(
-        onMapCreated: (controller) {
-          setState(() {
-            mapController = controller;
-          });
-        },
-        initialCameraPosition: CameraPosition(
-          target: LatLng(0.0, 0.0),
-          zoom: 2,
-        ),
-        markers: markers,
-        onTap: (latLng) {
-          setState(() {
-            selectedLatLng = latLng;
-            markers.clear();
-            markers.add(
-              Marker(
-                markerId: MarkerId('selectedLocation'),
-                position: selectedLatLng!,
-                infoWindow: InfoWindow(
-                  title: 'Seçilen Konum',
-                ),
-              ),
-            );
-          });
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Konumu seç ve kapat
-          if (selectedLatLng != null) {
-            Navigator.pop(
-              context,
-              '(${selectedLatLng!.latitude}, ${selectedLatLng!.longitude})',
-            );
-          }
-        },
-        child: Icon(Icons.check),
-      ),
-    );
+    // Konum izni al
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Eğer izin verilmezse hata ver
+        throw 'Konum izni verilmedi';
+      }
+    }
+
+    // Mevcut konumu al
+    return await Geolocator.getCurrentPosition();
   }
 }
